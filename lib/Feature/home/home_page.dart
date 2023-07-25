e459274ddcf0156a0f7edd9b4cf55fc35d46050e
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-07-25 15:44:46 +0530

    Added: Completed Signup/Login screen

--- lib/Feature/home/home_page.dart
+++ lib/Feature/home/home_page.dart
@@ -8,117 +8,119 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Expanded(
-            child: Column(
-          children: [
-            Image.asset(
-              'assets/png/onboarding2.png',
-              fit: BoxFit.fitWidth,
-              height: 450,
-              width: 500,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Find a dream holiday\n',
-                    style: TextStyle(
-                        // fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontFamily: 'PoppinsBold'),
-                  ),
-                  TextSpan(
-                    text: 'home',
-                    style: TextStyle(
-                        // fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontFamily: 'PoppinsBold'),
-                  ),
-                ],
+        body: SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              Image.asset(
+                'assets/png/onboarding2.png',
+                fit: BoxFit.fitWidth,
+                height: 450,
+                width: 500,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupLogin()),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: 'PoppinsReg'),
+              const SizedBox(
+                height: 25,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Find a dream holiday\n',
+                      style: TextStyle(
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontFamily: 'PoppinsBold'),
+                    ),
+                    TextSpan(
+                      text: 'home',
+                      style: TextStyle(
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontFamily: 'PoppinsBold'),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupLogin()),
-                );
-              },
-              child: Container(
+              const SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupLogin()),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'PoppinsReg'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupLogin()),
+                  );
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 320,
+                  height: 55,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: colors.yellow),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Next",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontFamily: 'PoppinsBold', fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
                 alignment: Alignment.center,
                 width: 320,
                 height: 55,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: colors.yellow),
+                  border: Border.all(color: colors.yellow, width: 2.0),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(255, 255, 235, 153),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(10),
                   child: Text(
-                    "Next",
+                    "Skip",
                     textAlign: TextAlign.center,
                     style: TextStyle(fontFamily: 'PoppinsBold', fontSize: 20),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: 320,
-              height: 55,
-              decoration: BoxDecoration(
-                border: Border.all(color: colors.yellow, width: 2.0),
-                borderRadius: BorderRadius.circular(30),
-                color: Color.fromARGB(255, 255, 235, 153),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  "Skip",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontFamily: 'PoppinsBold', fontSize: 20),
-                ),
-              ),
-            ),
-          ],
-        ))
-        // TextSpan()
-      ],
+            ],
+          ))
+          // TextSpan()
+        ],
+      ),
     ));
   }
 }
--- lib/Feature/signup-login/signup_login.dart
+++ lib/Feature/signup-login/signup_login.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:hola_home/Core/constants/colors.dart';
+import 'package:hola_home/Feature/signup-login/signup.dart';
 
 class SignupLogin extends StatelessWidget {
   const SignupLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/png/signup-login.png',
+            ),
+            Image.asset(
+              'assets/png/black_hola_home.png',
+              scale: 1.2,
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupLogin()),
+                    );
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 150,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: colors.yellow),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        "SignUp",
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontFamily: 'PoppinsBold', fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupLogin()),
+                    );
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 150,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: colors.yellow, width: 2.0),
+                      borderRadius: BorderRadius.circular(30),
+                      color: Color.fromARGB(255, 255, 235, 153),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        "Login",
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontFamily: 'PoppinsBold', fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Signup()),
+                );
+              },
+              child: Text(
+                "Skip and Start Browsing",
+                style: TextStyle(
+                    fontFamily: 'PoppinsReg',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
